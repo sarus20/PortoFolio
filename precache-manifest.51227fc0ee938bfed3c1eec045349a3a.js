0a70f76e590123ca4927b41264189e8e866cda87
sarumathiramasamy <43061815+sarumathiramasamy@users.noreply.github.com>
2020-06-07 23:48:50 +0800

    Updates

---
+++ precache-manifest.51227fc0ee938bfed3c1eec045349a3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d51970706c82d346ae35cc057563e7",
+    "url": "/PortoFolio/index.html"
+  },
+  {
+    "revision": "eb03362b8aba8f860075",
+    "url": "/PortoFolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "aa392aaefac929defdd4",
+    "url": "/PortoFolio/static/css/main.7eee9755.chunk.css"
+  },
+  {
+    "revision": "eb03362b8aba8f860075",
+    "url": "/PortoFolio/static/js/2.47b58693.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/PortoFolio/static/js/2.47b58693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa392aaefac929defdd4",
+    "url": "/PortoFolio/static/js/main.e3849c76.chunk.js"
+  },
+  {
+    "revision": "dae00bb7e45ed3b3e226",
+    "url": "/PortoFolio/static/js/runtime-main.dbccf014.js"
+  }
+]);
